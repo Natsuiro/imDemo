ace518033e49930b457cfd57e7ffdc388b1d2abf
hrh <1141120715@qq.com>
2020-04-24 15:00:01 +0800

    Splash View level completed

---
+++ app/src/main/java/com/szm/im/ui/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.szm.im.ui
+
+import com.szm.im.R
+
+class LoginActivity : BaseActivity() {
+
+    override fun getLayoutResId(): Int =
+        R.layout.activity_login
+}
--- app/src/main/java/com/szm/im/ui/SplashActivity.kt
+++ app/src/main/java/com/szm/im/ui/SplashActivity.kt
@@ -1,13 +1,36 @@
 package com.szm.im.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Handler
 import com.szm.im.R
+import com.szm.im.contract.SplashContract
+import org.jetbrains.anko.startActivity
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(),SplashContract.View {
 
-    //let BaseActivity inflate this layout with id
+    companion object{
+        const val DELAY = 2000L
+    }
+    //when handler be used at first time,the Handler() behind lazy will be exec
+    private val handler by lazy {
+        Handler()
+    }
+
+    //let the BaseActivity inflate this layout with id
     override fun getLayoutResId(): Int =
         R.layout.activity_splash
+
+    //delay 2s then jump to login page
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            //jump to login
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)
+    }
+    //jump to main page
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
+    }
 
 }
