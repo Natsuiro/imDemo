e0a80985095ab2db2dc6dce87e63474d8d6379c8
hrh <1141120715@qq.com>
2020-04-24 20:18:10 +0800

    register bmob

--- app/src/main/java/com/szm/im/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/szm/im/presenter/RegisterPresenter.kt
@@ -1,29 +1,51 @@
 package com.szm.im.presenter
 
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.SaveListener
+import com.hyphenate.chat.a.e
 import com.szm.im.contract.RegisterContract
 import com.szm.im.extensions.isValidPassword
 import com.szm.im.extensions.isValidUserName
 
-class RegisterPresenter(val view:RegisterContract.View): RegisterContract.Presenter{
+class RegisterPresenter(val view: RegisterContract.View) : RegisterContract.Presenter {
     override fun register(userName: String, password: String, confirmPassword: String) {
         //check username
-        if (userName.isValidUserName()){
+        if (userName.isValidUserName()) {
             //check password
-            if (password.isValidPassword()){
+            if (password.isValidPassword()) {
 
                 //check Confirm password
-                if (confirmPassword == password){
+                if (confirmPassword == password) {
                     //password equals to confirm password
                     view.onStartRegister()
                     //begin register
+                    registerBmob(userName, password)
 
+                } else view.onConfirmPasswordError()
 
+            } else view.onPasswordError()
 
-                }else view.onConfirmPasswordError()
+        } else view.onUserNameError()
+    }
 
-            }else view.onPasswordError()
+    private fun registerBmob(userName: String, password: String) {
+        val bu = BmobUser()
+        bu.username = userName
+        bu.setPassword(password)
 
-        }else view.onUserNameError()
+        bu.signUp<BmobUser>(object : SaveListener<BmobUser>() {
+            override fun done(s: BmobUser?, e: BmobException?) {
+                if (e == null) {
+                    //注册成功
+
+                } else {
+                    //注册失败
+                    view.onRegisterFailed()
+                }
+            }
+
+        })
     }
 
 }
