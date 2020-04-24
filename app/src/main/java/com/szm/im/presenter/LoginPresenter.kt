2606112941c5f2f8be8c8c0496b34ade80d578e6
hrh <1141120715@qq.com>
2020-04-24 16:56:13 +0800

    Login page Model Completed

--- app/src/main/java/com/szm/im/presenter/LoginPresenter.kt
+++ app/src/main/java/com/szm/im/presenter/LoginPresenter.kt
@@ -1,5 +1,9 @@
 package com.szm.im.presenter
 
+import android.util.Log
+import com.hyphenate.EMCallBack
+import com.hyphenate.chat.EMClient
+import com.szm.im.adapter.EMCallBackAdapter
 import com.szm.im.contract.LoginContract
 import com.szm.im.extensions.isValidPassword
 import com.szm.im.extensions.isValidUserName
@@ -10,7 +14,7 @@ class LoginPresenter(private val view:LoginContract.View) : LoginContract.Presen
         if (userName.isValidUserName()){
             //用户名合法，继续校验password
             if (password.isValidPassword()){
-                //密码合法
+                //密码合法,通知ui反馈用户
                 view.onStartLogin()
                 loginEaseMob(userName,password)
             }else{
@@ -25,7 +29,23 @@ class LoginPresenter(private val view:LoginContract.View) : LoginContract.Presen
      * 登录到环信服务器
      */
     private fun loginEaseMob(userName: String, password: String) {
+        EMClient.getInstance().login(userName,password,object : EMCallBackAdapter() {
+            //在子线程回调
+            override fun onSuccess() {
+                //在主线程中通知view层
+                EMClient.getInstance().groupManager().loadAllGroups()
+                EMClient.getInstance().chatManager().loadAllConversations()
+                Log.d("login","login success!")
+                //切换到主线程
+                uiThread { view.onLoggedInSuccess() }
+            }
 
+            override fun onError(p0: Int, p1: String?) {
+                //切换到主线程
+                uiThread { view.onLoggedInFailed() }
+            }
+
+        })
     }
 
 }
