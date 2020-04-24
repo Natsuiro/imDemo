83ca7c413b46fa6217819f014386cdb112b62306
hrh <1141120715@qq.com>
2020-04-24 14:36:05 +0800

    Splash layout completed

---
+++ app/src/main/java/com/szm/im/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.szm.im.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.szm.im.R
+
+class SplashActivity : BaseActivity() {
+
+    //let BaseActivity inflate this layout with id
+    override fun getLayoutResId(): Int =
+        R.layout.activity_splash
+
+}
