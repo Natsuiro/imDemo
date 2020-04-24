d75f8d5df8c2426dd1b8b11d331de847f21900ae
hrh <1141120715@qq.com>
2020-04-24 17:58:23 +0800

    Register page View completed

--- app/src/main/java/com/szm/im/ui/RegisterActivity.kt
+++ app/src/main/java/com/szm/im/ui/RegisterActivity.kt
@@ -1,9 +1,38 @@
 package com.szm.im.ui
 import com.szm.im.R
+import com.szm.im.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
 
-class RegisterActivity : BaseActivity() {
+class RegisterActivity : BaseActivity(),RegisterContract.View{
 
     override fun getLayoutResId(): Int {
         return R.layout.activity_register
+    }
+
+    override fun onUserNameError() {
+        username.error = getString(R.string.invalid_username)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.invalid_password)
+    }
+
+    override fun onConfirmPassword() {
+        confirm_password.error = getString(R.string.invalid_confirm_password)
+    }
+
+    override fun onStartRegister() {
+        showProgressDialog(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dismissProgressDialog()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        dismissProgressDialog()
+        toast(R.string.register_fail)
     }
 }
