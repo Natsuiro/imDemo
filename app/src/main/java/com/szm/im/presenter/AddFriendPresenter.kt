02b3da76d0cebf9e9104bc99f1908c9a98c161a5
hrh <1141120715@qq.com>
2020-04-25 16:17:46 +0800

    AddFriendActivity model init

--- app/src/main/java/com/szm/im/presenter/AddFriendPresenter.kt
+++ app/src/main/java/com/szm/im/presenter/AddFriendPresenter.kt
@@ -1,10 +1,30 @@
 package com.szm.im.presenter
 
+import android.webkit.WebView
+import cn.bmob.v3.BmobQuery
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.FindListener
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.a.e
 import com.szm.im.contract.AddFriendContract
 
 class AddFriendPresenter(val view : AddFriendContract.View) : AddFriendContract.Presenter{
     override fun search(key: String) {
 
+        val query = BmobQuery<BmobUser>()
+        query.addWhereContains("username",key)
+            .addWhereNotEqualTo("username",EMClient.getInstance().currentUser)
+        query.findObjects(object : FindListener<BmobUser>(){
+            override fun done(p0: MutableList<BmobUser>?, e: BmobException?) {
+                if (e == null){
+                    view.onSearchSuccess()
+                }else{
+                    view.onSearchFailed()
+                }
+            }
+
+        })
     }
 
 }
