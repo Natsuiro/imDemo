81fa720b40877dc3bd7a0122cc0b446614e638bd
hrh <1141120715@qq.com>
2020-04-25 10:50:09 +0800

    Dynamic layout

--- app/src/main/java/com/szm/im/ui/fragment/DynamicFragment.kt
+++ app/src/main/java/com/szm/im/ui/fragment/DynamicFragment.kt
@@ -1,10 +1,24 @@
 package com.szm.im.ui.fragment
 
+import com.hyphenate.chat.EMClient
 import com.szm.im.R
+import kotlinx.android.synthetic.main.fragment_dynamic.*
+import kotlinx.android.synthetic.main.header.*
 
 class DynamicFragment : BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_dynamic
+    }
+
+    override fun init() {
+        super.init()
+        header_title.text = getString(R.string.dynamic)
+        username.text = EMClient.getInstance().currentUser
+
+        log_out.setOnClickListener {
+
+        }
+
     }
 
 }
