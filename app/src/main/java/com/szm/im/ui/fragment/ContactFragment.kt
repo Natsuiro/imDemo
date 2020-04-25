bde52fc20454656679687ccdea6b9ff2393c6240
hrh <1141120715@qq.com>
2020-04-25 11:05:02 +0800

    Contact layout init

--- app/src/main/java/com/szm/im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/szm/im/ui/fragment/ContactFragment.kt
@@ -1,10 +1,24 @@
 package com.szm.im.ui.fragment
 
+import android.view.View
 import com.szm.im.R
+import kotlinx.android.synthetic.main.fragment_contact.*
+import kotlinx.android.synthetic.main.header.*
 
 class ContactFragment : BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_contact
+    }
+
+    override fun init() {
+        super.init()
+        header_title.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+
+        swipeRefreshLayout.apply {
+            setColorSchemeColors(resources.getColor(R.color.im_blue))
+            isRefreshing = true
+        }
     }
 
 }
