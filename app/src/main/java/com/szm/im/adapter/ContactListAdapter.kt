d73ff233ef3b8b676b2681f751351cf7be6da663
hrh <1141120715@qq.com>
2020-04-25 14:00:59 +0800

    删除好友逻辑

--- app/src/main/java/com/szm/im/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/szm/im/adapter/ContactListAdapter.kt
@@ -7,11 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.hyphenate.chat.EMClient
 import com.szm.im.R
 import com.szm.im.data.ContactListItem
 import com.szm.im.ui.activity.ChatActivity
 import com.szm.im.widget.ContactListItemView
+import org.jetbrains.anko.runOnUiThread
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class ContactListAdapter(private val context:Context,private val contactListItems: MutableList<ContactListItem>):RecyclerView.Adapter<ContactListAdapter.ContactItemViewHolder>() {
 
@@ -51,6 +54,23 @@ class ContactListAdapter(private val context:Context,private val contactListItem
 
     private fun deleteFriend(userName: String) {
         Log.d(tag,"delete friend!")
+        //doAsync
+        EMClient.getInstance().contactManager().aysncDeleteContact(userName,object :
+            EMCallBackAdapter() {
+            override fun onSuccess() {
+                context.runOnUiThread {
+                    toast(R.string.delete_friend_success)
+                }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                context.runOnUiThread {
+                    toast(R.string.delete_friend_failed)
+                }
+            }
+
+        })
+
     }
 
     class ContactItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
