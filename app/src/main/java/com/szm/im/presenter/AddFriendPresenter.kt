7ac060783f8a37cc3c830cbc91251a8028fdd340
hrh <1141120715@qq.com>
2020-04-25 16:09:51 +0800

    AddFriendActivity presenter init

---
+++ app/src/main/java/com/szm/im/presenter/AddFriendPresenter.kt
@@ -0,0 +1,10 @@
+package com.szm.im.presenter
+
+import com.szm.im.contract.AddFriendContract
+
+class AddFriendPresenter(val view : AddFriendContract.View) : AddFriendContract.Presenter{
+    override fun search(key: String) {
+
+    }
+
+}
--- app/src/main/java/com/szm/im/ui/activity/AddFriendActivity.kt
+++ app/src/main/java/com/szm/im/ui/activity/AddFriendActivity.kt
@@ -2,10 +2,13 @@ package com.szm.im.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.szm.im.R
 import com.szm.im.adapter.AddFriendListAdapter
 import com.szm.im.contract.AddFriendContract
+import com.szm.im.presenter.AddFriendPresenter
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
 import org.jetbrains.anko.toast
@@ -15,6 +18,8 @@ class AddFriendActivity : BaseActivity(),AddFriendContract.View {
         return R.layout.activity_add_friend
     }
 
+    val presenter = AddFriendPresenter(this)
+
     override fun init() {
         header_title.text = getString(R.string.add_friend)
 
@@ -23,6 +28,21 @@ class AddFriendActivity : BaseActivity(),AddFriendContract.View {
             layoutManager = LinearLayoutManager(context)
             adapter = AddFriendListAdapter(context)
         }
+
+        search.setOnClickListener {
+            search()
+        }
+        username.setOnEditorActionListener { _, _, _ ->
+            search()
+            true
+        }
+    }
+
+    private fun search(){
+        hideSoftKeyBoard()
+        showProgressDialog(getString(R.string.searching))
+        val key = username.text.trim().toString()
+        presenter.search(key)
     }
 
     override fun onSearchSuccess() {
