69e757100dfa1b6e15bd585e766ca88160339c23
hrh <1141120715@qq.com>
2020-04-25 11:00:21 +0800

    Dynamic log out

--- app/src/main/java/com/szm/im/ui/fragment/DynamicFragment.kt
+++ app/src/main/java/com/szm/im/ui/fragment/DynamicFragment.kt
@@ -2,8 +2,13 @@ package com.szm.im.ui.fragment
 
 import com.hyphenate.chat.EMClient
 import com.szm.im.R
+import com.szm.im.adapter.EMCallBackAdapter
+import com.szm.im.ui.activity.LoginActivity
 import kotlinx.android.synthetic.main.fragment_dynamic.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class DynamicFragment : BaseFragment() {
     override fun getLayoutResId(): Int {
@@ -16,9 +21,30 @@ class DynamicFragment : BaseFragment() {
         username.text = EMClient.getInstance().currentUser
 
         log_out.setOnClickListener {
-
+            logOut()
         }
 
     }
 
+    private fun logOut(){
+        EMClient.getInstance().logout(true,object : EMCallBackAdapter(){
+            override fun onSuccess() {
+
+                context?.runOnUiThread {
+                    toast(R.string.log_out_success)
+                    context?.startActivity<LoginActivity>()
+                    activity?.finish()
+                }
+
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                //thread
+                context?.runOnUiThread {
+                    toast(R.string.log_out_failed)
+                }
+            }
+
+        })
+    }
 }
