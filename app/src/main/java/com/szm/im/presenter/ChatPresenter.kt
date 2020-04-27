488c9c8cfa482dcecc8b695bfb6a4cd09e5bd7b7
hrh <1141120715@qq.com>
2020-04-27 08:25:27 +0800

    发送消息的mvp Model

--- app/src/main/java/com/szm/im/presenter/ChatPresenter.kt
+++ app/src/main/java/com/szm/im/presenter/ChatPresenter.kt
@@ -1,10 +1,30 @@
 package com.szm.im.presenter
 
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
+import com.szm.im.adapter.EMCallBackAdapter
 import com.szm.im.contract.ChatContract
 
 class ChatPresenter(val view:ChatContract.View) : ChatContract.Presenter {
-    override fun sendMessage(contact: String, message: String) {
 
+    val messages = mutableListOf<EMMessage>()
+
+    override fun sendMessage(contact: String, message: String) {
+        val emMessage = EMMessage.createTxtSendMessage(message,contact)
+
+        emMessage.setMessageStatusCallback(object : EMCallBackAdapter(){
+            override fun onSuccess() {
+                uiThread { view.onSendMessageSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onSendMessageFailed() }
+            }
+        })
+
+        messages.add(emMessage)
+        view.onStartSendMessage()
+        EMClient.getInstance().chatManager().sendMessage(emMessage)
     }
 
 }
