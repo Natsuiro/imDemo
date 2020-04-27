9fbd047f7d484fa9b3d43a6b28a24cf52237882b
hrh <1141120715@qq.com>
2020-04-27 15:55:30 +0800

    Conversation fragment layout init

--- app/src/main/java/com/szm/im/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/szm/im/ui/fragment/ConversationFragment.kt
@@ -1,10 +1,16 @@
 package com.szm.im.ui.fragment
 
 import com.szm.im.R
+import kotlinx.android.synthetic.main.header.*
 
 class ConversationFragment : BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_conversation
+    }
+
+    override fun init() {
+        super.init()
+        header_title.text = getString(R.string.message)
     }
 
 }
