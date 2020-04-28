3ca9105375f7e2fe0cb381e6cc0adfc6994c52c9
hrh <1141120715@qq.com>
2020-04-28 16:05:59 +0800

    notification

--- app/src/main/java/com/szm/im/app/IMApplication.kt
+++ app/src/main/java/com/szm/im/app/IMApplication.kt
@@ -1,12 +1,19 @@
 package com.szm.im.app
 
+import android.app.ActivityManager
 import android.app.Application
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Build
 import cn.bmob.v3.Bmob
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMMessage
 import com.hyphenate.chat.EMOptions
+import com.hyphenate.chat.EMTextMessageBody
 import com.szm.im.BuildConfig
+import com.szm.im.R
 import com.szm.im.adapter.EMMessageListenerAdapter
 
 class IMApplication : Application() {
@@ -17,9 +24,32 @@ class IMApplication : Application() {
 
     private val messageListener = object : EMMessageListenerAdapter() {
         override fun onMessageReceived(p0: MutableList<EMMessage>?) {
-
+            if (!isForeGround()){
+                showNotification(p0)
+            }
         }
     }
+
+    private fun showNotification(p0: MutableList<EMMessage>?) {
+
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        p0?.forEach {
+            var contentText = getString(R.string.not_text_message)
+
+            if (it.type == EMMessage.Type.TXT){
+                contentText = (it.body as EMTextMessageBody).message
+            }
+
+           val notification = Notification.Builder(this)
+               .setContentTitle(it.userName)
+               .setContentText(contentText)
+               .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.icon_avatar))
+               .setSmallIcon(R.drawable.icon_avatar)
+               .notification
+            manager.notify(1,notification)
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         instance = this
@@ -39,6 +69,15 @@ class IMApplication : Application() {
         EMClient.getInstance().chatManager().addMessageListener(messageListener)
     }
 
+    private fun isForeGround():Boolean{
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.runningAppProcesses.forEach {
+            if (it.processName == packageName){
+               return it.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+            }
+        }
+        return false
+    }
 
 
 }
